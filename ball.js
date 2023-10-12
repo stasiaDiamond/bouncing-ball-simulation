36a876d3905e995c36baacc35b900e09d80dceb5
Nastasia <nsisco@uw.edu>
2023-10-12 15:34:26 -0700

    updates comments to be more helpful

--- ball.js
+++ ball.js
@@ -4,9 +4,11 @@ var ball2 = document.getElementById("ball2");
 var ball3 = document.getElementById("ball3");
 var ball4 = document.getElementById("ball4");
 
-// create variables for the velocity and position of small balls
+// create variables for the velocity of all balls
 var velocityX = 4;
 var velocityY = 2;
+
+// create variables for starting position of small balls
 var positionX = 0;
 var positionY = 0;
 
@@ -20,7 +22,7 @@ var Xmax = 650;
 var Ymin = 0;
 var Ymax = 200;
 
-// create variables to declare which direction to move the balls first
+// create variables to declare which direction to move the balls when they start
 // positive for right, negative for left
 var directionX = 1;
 var directionY = 1;
@@ -41,7 +43,7 @@ function moveBall() {
   positionYbig += directionYbig * velocityY;
 
 
-  // Set the ball's position in the div and concatenate the number with px to make them pixels
+  // Set the ball's position in the div and concatenate the number with px to make them pixels so when HTML document reads it, it looks correct to it
   ball.style.left = positionX + "px";
   ball.style.top = positionY + "px";
 
@@ -54,6 +56,11 @@ function moveBall() {
   // big ball
   ball4.style.left = positionXbig + "px";
   ball4.style.top = positionYbig + "px";
+  // ball.style.left broken down:
+  // ball.____.___ states we're dealing with an element we gave the id of ball on line 1
+  // ____.style.____ states we're accessing the inline CSS properties of the element we named with the id "ball". Go into your CSS file and find #ball ( an id in a CSS file looks like this # )
+  // ___.___.left in your CSS file under #ball, one of it's key value pairs is left: 0px. This states that we're about to change that to whatever we type after the equals sign ( assignment singular = )
+
 
   // if the lead small balls have reached the x and y-axis boundaries, reverse direction by setting it to (-)
   if (positionX >= Xmax || positionX <= Xmin) {
@@ -77,7 +84,12 @@ function moveBall() {
 // Call the moveBall function every 10ms
 setInterval(moveBall, 10);
 
-// Codio assignment was extremely misleading. Finally got it to work with this:
+
+
+// The Codio assignment for this Module activity was extremely misleading and many of my classmates were frustrated. The test suites did not allow many correct functions to pass. It was quite the surprise to find that even copy pasta of the "Solved File" didn't pass the test suites either. 
+// I finally got it to work with the below code. I hope this helps someone in the future!
+
+
 // var ball = document.getElementById("ball");
 // var velocity = 100;
 // var positionX = 0;
